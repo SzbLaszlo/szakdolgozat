a04c64cacc1232b81836f804e86cbec36a102c6d
SzbLaszlo <szabo.laszlo.2017ice@bankitatabanya.hu>
2022-01-23 21:10:09 +0100

    form

---
+++ regisztral.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <form action="regisztral.php" method="POST">
+            Felhasználó név: <input type="text" name="felhasznalo">
+            Jelszó: <input type="password" name="jelszo">
+            Jelszó újra: <input type="password" name="jelszoujra">
+            <input value="Regisztráció" type="submit" name="submit">
+        </form>
+    </body>
+</html>
