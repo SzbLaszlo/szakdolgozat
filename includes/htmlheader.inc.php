43019d250ecbd3f2e6647733498ca242f7716f8f
SzbLaszlo <szabo.laszlo.2017ice@bankitatabanya.hu>
2022-03-21 23:17:29 +0100

    képek

--- includes/htmlheader.inc.php
+++ includes/htmlheader.inc.php
@@ -2,8 +2,10 @@
 <html lang="hu">
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $title; ?></title>	
+	<title><?php echo $title; ?></title>
+    <link rel="icon" type="image/x-icon" href="kepek/favicon.png">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 </head>
+</html>
